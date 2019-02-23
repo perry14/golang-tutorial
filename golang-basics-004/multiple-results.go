7d4329d08011f8127b1a1cdbd653af27cb6eddda
perry14 <yuezuofang@vip.qq.com>
2019-02-23 09:12:54 +0800

    multiple-results demo

---
+++ golang-basics-004/multiple-results.go
@@ -0,0 +1,13 @@
+// package project multiple-results.go
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	a, b := swap("Hello", "World")
+	fmt.Println(a, b)
+}
